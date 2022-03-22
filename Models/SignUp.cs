5326bc23102c779783ec58457b99b12bb6612b86
jkbehling <90301783+jkbehling@users.noreply.github.com>
2022-03-22 09:41:48 -0600

    Set up models and database

---
+++ Models/SignUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleSignup_Mission12_.Models
+{
+    public class SignUp
+    {
+        [Key]
+        [Required]
+        public int SignUpId { get; set; }
+        [Required]
+        public int GroupSize { get; set; }
+        [Required]
+        public string GroupName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int AppointmentId { get; set; }
+        public Appointment Appointment { get; set; }
+    }
+}
